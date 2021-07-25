3b79218666fccf755bc8c1e502b04d66bf662410
acumpstone <a.cumpstone@outlook.com>
2021-07-25 16:49:29 -0230

    Added custom name route

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("hello.html")
+
+@app.route("/<name>")
+def hello_name(name):
+    return f"<p>Hello, {name}!</p>"
