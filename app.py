cebb3ab21417d62b6604e75477e92a5ec5215219
acumpstone <a.cumpstone@outlook.com>
2021-07-26 20:46:38 -0230

    Followed up to the API section of Dev Toolkit video. Got projects using json and github API

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
 from flask import Flask, render_template
+import requests
+
+name = "Alexis"
+
+github_projects_url = "https://api.github.com/users/acumpstone/repos"
+projects_from_github = requests.get(github_projects_url).json()
+
+projects = []
+
+for project in projects_from_github:
+    name = project["name"]
+    desc = project["description"]
+    url = project["html_url"]
+
+    projects.append({
+        "name": name,
+        "desc": desc,
+        "url": url,
+    })
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("hello.html")
+list_of_hobbies = {
+    "anime",
+    "piano",
+    "reading"
+}
 
-@app.route("/<name>")
-def hello_name(name):
-    return f"<p>Hello, {name}!</p>"
+@app.route("/")
+def about_page():
+    return render_template("about.html", name=name, list_of_hobbies=list_of_hobbies)
+
+@app.route("/projects")
+def projects_page():
+    return render_template("projects.html", name=name, projects=projects)
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html", name=name)
