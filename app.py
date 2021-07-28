506efd7ed83a6e7449e5f9ecd747051ed73de2a6
acumpstone <a.cumpstone@outlook.com>
2021-07-27 21:47:11 -0230

    Started to work on the blog page from Dev Toolkit Bootcamp session 3

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
+import os
+from pathlib import Path
+
 from flask import Flask, render_template
 import requests
+import decouple
 
 name = "Alexis"
 
 github_projects_url = "https://api.github.com/users/acumpstone/repos"
 projects_from_github = requests.get(github_projects_url).json()
 
+contact = decouple.config("CONTACT_FORM_API", default=None)
+
 projects = []
 
+blog_posts = []
+
+with os.scandir("blog") as it:
+    for entry in it:
+        if entry.name.endswith(".md") and entry.is_file():
+            post_data = Path(entry.path).read_text()
+
+            blog_posts.append({
+                "name": entry.name,
+                "data": post_data
+            })
+
+print(blog_posts)
+
+exit(0)
+
 for project in projects_from_github:
-    name = project["name"]
+    project_name = project["name"]
     desc = project["description"]
     url = project["html_url"]
 
     projects.append({
-        "name": name,
+        "name": project_name,
         "desc": desc,
         "url": url,
     })
@@ -31,10 +53,18 @@ list_of_hobbies = {
 def about_page():
     return render_template("about.html", name=name, list_of_hobbies=list_of_hobbies)
 
+@app.route("/blog")
+def blog_entry_page():
+    return render_template("blog_listing.html", name=name)
+
+@app.route("/blog/<post_name>")
+def blog_listing_page(post_name):
+    return render_template("blog_entry.html", name=name, post_name=post_name)
+
 @app.route("/projects")
 def projects_page():
     return render_template("projects.html", name=name, projects=projects)
 
 @app.route("/contact")
 def contact_page():
-    return render_template("contact.html", name=name)
+    return render_template("contact.html", name=name, api=contact)
---
+++ blog/second_post.md
@@ -0,0 +1,13 @@
+# Second post!
+
+<('-')>
+
+text and stuff
+text and stuff
+text and stuff
+
+1. text and stuff
+2. text and stuff
+3. text and stuff
+4. text and stuff
+5. text and stuff
